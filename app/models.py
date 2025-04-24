5e34fdc267dc84c511f75f9fde47e83bd21c1216
saacutter <168063553+saacutter@users.noreply.github.com>
2025-04-24 12:48:32 +0800

    Added 'users' table to database

--- app/models.py
+++ app/models.py
@@ -1,7 +1,14 @@
 from app import db
+from sqlalchemy import Integer, Text
+from sqlalchemy.orm import Mapped, mapped_column
+from typing import Optional
+from datetime import datetime, timezone
 
 class Users(db.Model):
-    id         = db.Column(db.Integer, primary_key=True, nullable=False)
-    username   = db.Column(db.Text, nullable=False, unique=True)
-    password   = db.Column(db.Text, nullable=False)
-    last_login = db.Column(db.DateTime(timezone=False), nullable=False)
+    id:         Mapped[int]      = mapped_column(Integer, primary_key=True)
+    username:   Mapped[str]      = mapped_column(Text, unique=True, nullable=False, index=True)
+    password:   Mapped[str]      = mapped_column(Text, nullable=False, index=True)
+    last_login: Mapped[datetime] = mapped_column(nullable=False, index=True, default=lambda: datetime.now(timezone.utc))
+
+    def __rpr__(self):
+        return f"{self.id} {self.username}"
