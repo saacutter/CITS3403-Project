31b2aab3c73e73fbfa175d5a9410ed047443b605
saacutter <168063553+saacutter@users.noreply.github.com>
2025-04-24 12:46:25 +0800

    Removed legacy database files

--- app/database/schema.sql
+++
@@ -1,7 +0,0 @@
--- Create Users table
-CREATE TABLE Users (
-    id          INTEGER PRIMARY KEY NOT NULL,
-    username    TEXT UNIQUE NOT NULL,
-    password    TEXT NOT NULL,
-    last_login  DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
