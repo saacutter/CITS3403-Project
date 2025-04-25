7c93dd13652637701e029417df4436e3ff80cc6a
saacutter <168063553+saacutter@users.noreply.github.com>
2025-04-25 21:52:08 +0800

    Adjusted styling of the login page (mostly font change but also the colour of buttons)

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from app import application, db, migrate, models
+from app import application, db, migrate, models, forms
 from flask import render_template, request, redirect, url_for, session
 
 # Default route of the application
@@ -9,15 +9,15 @@ def index():
 # Login route of the application
 @application.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == "POST":
+    if request.method == "POST" and forms.LoginForm().validate_on_submit():
         ... # TODO: Create the sign in logic
         return redirect("/")
-    return render_template("login.html", signed_out=True, login=True) # Display login page
+    return render_template("login.html", signed_out=True, login=True, loginForm=forms.LoginForm(), signupForm=forms.SignupForm()) # Display login page
         
 # Signup route of the application
 @application.route("/signup", methods=["GET", "POST"])
 def signup():
-    if request.method == "POST":
+    if request.method == "POST" and forms.SignupForm().validate_on_submit():
         ... # TODO: Create the sign up logic
         return redirect("/")
-    return render_template("login.html", signed_out=True, login=False) # Display sign up page
+    return render_template("login.html", signed_out=True, login=False, loginForm=forms.LoginForm(), signupForm=forms.SignupForm()) # Display sign up page
