f1259509ae542c5a4c43211534e70a4b96c5098b
saacutter <168063553+saacutter@users.noreply.github.com>
2025-04-25 21:51:37 +0800

    Added WTForms to login page

--- app/forms.py
+++ app/forms.py
@@ -1,2 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, BooleanField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    username      = StringField('Username', validators=[DataRequired()])
+    password      = PasswordField('Password', validators=[DataRequired()])
+    remember_user = BooleanField('Remember Me')
+    submit        = SubmitField('Log In')
+
+class SignupForm(FlaskForm):
+    username         = StringField('Username', validators=[DataRequired()])
+    email            = StringField('Email Address', validators=[DataRequired()])
+    password         = PasswordField('Password', validators=[DataRequired()])
+    password_confirm = PasswordField('Confirm Password', validators=[DataRequired()])
+    submit           = SubmitField('Sign Up')
