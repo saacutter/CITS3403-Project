bafde7bbccc8e5ad68d7a3ec62f166d54d701981
saacutter <168063553+saacutter@users.noreply.github.com>
2025-04-26 21:19:52 +0800

    Adjusted SignupForm form to validate the email and confirm the password

--- app/forms.py
+++ app/forms.py
@@ -1,16 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, BooleanField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo
 
 class LoginForm(FlaskForm):
-    username      = StringField('Username', validators=[DataRequired()])
+    username      = StringField('Username or Email Address', validators=[DataRequired()])
     password      = PasswordField('Password', validators=[DataRequired()])
     remember_user = BooleanField('Remember Me')
     submit        = SubmitField('Log In')
 
 class SignupForm(FlaskForm):
     username         = StringField('Username', validators=[DataRequired()])
-    email            = StringField('Email Address', validators=[DataRequired()])
+    email            = StringField('Email Address', validators=[DataRequired(), Email()])
     password         = PasswordField('Password', validators=[DataRequired()])
-    password_confirm = PasswordField('Confirm Password', validators=[DataRequired()])
+    password_confirm = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit           = SubmitField('Sign Up')
