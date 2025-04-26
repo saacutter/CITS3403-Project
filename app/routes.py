96b3e2d354612e6dd751ce7c7f8cb5ba5138eb48
saacutter <168063553+saacutter@users.noreply.github.com>
2025-04-26 09:57:34 +0800

    Added random image to the signed in user homepage for testing purposes

--- app/routes.py
+++ app/routes.py
@@ -3,15 +3,17 @@ from flask import render_template, request, redirect, url_for, session
 
 # Default route of the application
 @application.route("/")
+@application.route("/index")
 def index():
-    return render_template("index.html", signed_out=True)
+    user = {'username': 'username', 'image': 'https://picsum.photos/200'} # This is just a temporary user where the image is a random image
+    return render_template("index.html", signed_out=True, user=user) # TODO: signed_out can be assigned to the .is_authenticated method when we come to that
 
 # Login route of the application
 @application.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST" and forms.LoginForm().validate_on_submit():
         ... # TODO: Create the sign in logic
-        return redirect("/")
+        return redirect(url_for('index'))
     return render_template("login.html", signed_out=True, login=True, loginForm=forms.LoginForm(), signupForm=forms.SignupForm()) # Display login page
         
 # Signup route of the application
@@ -19,5 +21,5 @@ def login():
 def signup():
     if request.method == "POST" and forms.SignupForm().validate_on_submit():
         ... # TODO: Create the sign up logic
-        return redirect("/")
+        return redirect(url_for('index'))
     return render_template("login.html", signed_out=True, login=False, loginForm=forms.LoginForm(), signupForm=forms.SignupForm()) # Display sign up page
