ea4a76c8f8f53e0a7396a5b7b3b01c6188548388
saacutter <168063553+saacutter@users.noreply.github.com>
2025-04-27 21:05:11 +0800

    Added Tournaments table to database to enable data to be uploaded by users

--- app/models.py
+++ app/models.py
@@ -1,8 +1,8 @@
 from app import db, login
-from sqlalchemy import Integer, Text
+from sqlalchemy import Integer, Text, ForeignKey, Enum
 from sqlalchemy.orm import Mapped, mapped_column
 from flask_login import UserMixin
-from datetime import datetime, timezone
+from datetime import datetime, timezone, time
 
 @login.user_loader
 def load_user(id):
@@ -18,3 +18,11 @@ class Users(db.Model, UserMixin):
 
     def __repr__(self):
         return f"{self.id} is {self.username} ({self.email}, {self.password}, {self.creation_date}, {self.last_login})"
+    
+class Tournaments(db.Model):
+    id:         Mapped[int]  = mapped_column(Integer, primary_key=True)
+    user_id:    Mapped[int]  = mapped_column(ForeignKey(Users.id), nullable=False, index=True)
+    game:       Mapped[str]  = mapped_column(Text, nullable=False, index=True)
+    points:     Mapped[int]  = mapped_column(Integer, nullable=False, index=True, default=0)
+    time_taken: Mapped[time] = mapped_column(nullable=False, index=True)
+    result:     Mapped[Enum] = mapped_column(Enum(win="win", loss="loss", draw="draw"), nullable=False, index=True)
