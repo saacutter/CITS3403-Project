1633e2a8d5396b2d452289f1e4c506d227b87a51
Arnav Kaul <arnavkaul@Arnavs-MacBook-Air.local>
2025-04-27 22:37:13 +0800

    Implement privacy setting field across signup form, user model, and templates

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, BooleanField, PasswordField, SubmitField
+from wtforms import StringField, BooleanField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo
 
 class LoginForm(FlaskForm):
@@ -12,10 +12,6 @@ class SignupForm(FlaskForm):
     username         = StringField('Username', validators=[DataRequired()])
     email            = StringField('Email Address', validators=[DataRequired(), Email()])
     password         = PasswordField('Password', validators=[DataRequired()])
-<<<<<<< HEAD
-    password_confirm = PasswordField('Confirm Password', validators=[DataRequired()])
-    privacy          = BooleanField('Make Profile Private?')
-=======
     password_confirm = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
->>>>>>> a1c9758316cbf4658d940029c20ce876bbc30683
+    privacy          = BooleanField('Make my profile private')
     submit           = SubmitField('Sign Up')
