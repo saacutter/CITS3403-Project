a83b8d044b977ac0faa7d9320d9b0316e38164f7
saacutter <168063553+saacutter@users.noreply.github.com>
2025-05-01 17:23:17 +0800

    Added Tournament form and replaced the old Tournament form with Matches

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, PasswordField, SubmitField, FileField
+from wtforms import StringField, BooleanField, PasswordField, SubmitField, FileField, DateTimeField
 from wtforms.validators import DataRequired, Email, EqualTo, Regexp
 from flask_wtf.file import FileAllowed
 
@@ -14,4 +14,20 @@ class SignupForm(FlaskForm):
     email            = StringField('Email Address', validators=[DataRequired(), Email(message="Enter a valid email address.")])
     password         = PasswordField('Password', validators=[DataRequired(), Regexp(r'^[a-zA-Z0-9!"#$%&\'()*+,-./:;<=>?@\[\\\]^_`{|}~]+$', message="Password must be at least 8 characters and contain only letters, numbers, and !@#$%^&*()_+=-")])
     password_confirm = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password',message="Passwords must match.")])
+    privacy          = BooleanField('Make my profile private')
     submit           = SubmitField('Sign Up')
+
+class AddMatchForm(FlaskForm):
+    file       = FileField('File Upload', validators=[FileAllowed(['json', 'csv'])])
+    game       = StringField('Game')
+    points     = StringField('Points')
+    time_taken = DateTimeField('Time Taken')
+    result     = StringField('Result')
+    submit     = SubmitField('Add Match')
+
+class AddTournamentForm(FlaskForm):
+    file   = FileField('File Upload', validators=[FileAllowed(['json', 'csv'])])
+    name   = StringField('Tournament Name')
+    game   = StringField('Game')
+    time   = DateTimeField('Date')
+    submit = SubmitField('Add Game')
