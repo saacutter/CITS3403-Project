9444fb38cd9702d0240bec5684c33f6b834a0e16
saacutter <168063553+saacutter@users.noreply.github.com>
2025-05-04 20:57:35 +0800

    Added a form (EditProfileForm) that allows users to edit their profiles

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, BooleanField, PasswordField, SubmitField
+from wtforms import StringField, BooleanField, PasswordField, SubmitField, FileField, DateTimeField
 from wtforms.validators import DataRequired, Email, EqualTo, Regexp
+from flask_wtf.file import FileAllowed
 
 class LoginForm(FlaskForm):
     username      = StringField('Username or Email Address', validators=[DataRequired()])
@@ -8,7 +9,7 @@ class LoginForm(FlaskForm):
     remember_user = BooleanField('Remember Me')
     submit        = SubmitField('Log In')
 
-class SignupForm(FlaskForm):
+class RegistrationForm(FlaskForm):
     username         = StringField('Username', validators=[DataRequired(),Regexp(r'^[a-zA-Z0-9]+$', message="Username must contain only letters and numbers.")])
     email            = StringField('Email Address', validators=[DataRequired(), Email(message="The email address must be a valid email address")])
     password         = PasswordField('Password', validators=[DataRequired(), Regexp(r'^[a-zA-Z0-9!"#$%&\'()*+,-./:;<=>?@\[\\\]^_`{|}~]+$', message="The password must contain only letters, numbers, and !@#$%^&*()_+=-")])
@@ -16,4 +17,25 @@ class SignupForm(FlaskForm):
     privacy          = BooleanField('Make my profile private')
     submit           = SubmitField('Sign Up')
 
+class AddMatchForm(FlaskForm):
+    file       = FileField('File Upload', validators=[FileAllowed(['json', 'csv'])])
+    game       = StringField('Game')
+    points     = StringField('Points')
+    time_taken = DateTimeField('Time Taken')
+    result     = StringField('Result')
+    submit     = SubmitField('Add Match')
 
+class AddTournamentForm(FlaskForm):
+    file   = FileField('File Upload', validators=[FileAllowed(['json', 'csv'])])
+    name   = StringField('Tournament Name')
+    game   = StringField('Game')
+    time   = DateTimeField('Date')
+    submit = SubmitField('Add Game')
+
+class EditProfileForm(FlaskForm):
+    username        = StringField('Username')
+    email           = StringField('Email Address')
+    password        = PasswordField('Password')
+    profile_picture = FileField('Profile Picture', validators=[FileAllowed(['jpeg', 'jpg', 'png', 'webp'])])
+    private         = BooleanField('Make my profile private')
+    submit          = SubmitField('Save Changes')
