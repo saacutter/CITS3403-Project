04d615ad2336afd52649ba58efbcc99ed2d66088
Arnav Kaul <arnavkaul@192.168.20.49>
2025-05-05 23:06:14 +0800

    Add image resolution limit to profile picture upload

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,7 @@ from werkzeug.utils import secure_filename
 from datetime import datetime, timezone
 from hashlib import md5
 import os
+from PIL import Image
 
 # Update the last seen time of the user before each request
 @application.before_request
@@ -135,7 +136,13 @@ def edit_profile():
         
         # Save the uploaded image to the server if one was uploaded
         img_filename = secure_filename(image.filename)
-        if img_filename != "":
+        if img_filename != "": 
+            img = Image.open(image)  
+            max_width, max_height = 500, 500 
+            if img.width > max_width or img.height > max_height:  
+                flash(f"The image resolution is too large. Max allowed is {max_width}x{max_height}px.")  
+                return redirect(url_for('edit_profile')) 
+
             # Ensure that the extension is a valid image extension
             extension = os.path.splitext(img_filename)[1]
             if extension not in application.config['UPLOAD_EXTENSIONS']:
