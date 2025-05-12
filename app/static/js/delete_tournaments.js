e695beb2a4c94a7850673105da6c767ef3eef487
saacutter <168063553+saacutter@users.noreply.github.com>
2025-05-12 22:22:21 +0800

    Added the 'delete_tournament' route to remove tournaments that the user specifies from the database

---
+++ app/static/js/delete_tournaments.js
@@ -0,0 +1,30 @@
+const tournamentsDiv = document.querySelector('#tournaments');
+const tournamentButtons = tournamentsDiv.querySelectorAll('button');
+
+for (let i = 0; i < tournamentButtons.length; i++) {
+    tournamentButtons[i].addEventListener('click', () => {
+        // Find the closest div
+        let tournament = tournamentButtons[i].closest('div');
+        let tDiv = tournamentButtons[i].closest('#tournament');
+
+        // Make the request to the designated route
+        var request = new XMLHttpRequest();
+        request.open('POST', '/delete_tournament/' + tournament.id, true);
+        request.onload = () => {
+            // Ensure that the request was successful
+            if (request.status == 200) {
+                // Remove the friend from the page
+                tournamentsDiv.removeChild(tDiv);
+
+                // Check if the user has any friends left and render the appropriate text
+                if (tournamentsDiv.querySelectorAll('div').length == 0) {
+                    let heading = document.querySelector('#tournaments').previousElementSibling;
+                    heading.insertAdjacentHTML('afterend', `
+                        <p class="text-white text-med">This user has not competed in any tournaments yet.</p>
+                    `);
+                }
+            }
+        };
+        request.send();
+    });
+}
